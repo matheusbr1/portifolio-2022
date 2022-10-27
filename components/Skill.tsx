bc7733fff6d30f65df7c028dc25435b515abe38e
Matheus Baron <matheusbaron10@gmail.com>
2022-10-27 17:17:29 -0300

    Fix: skill size on lg

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -46,9 +46,9 @@ export default function Skill({ direction = 'top', skill }: Props) {
       <img 
         src={urlFor(skill.image).url()}
         alt={skill.title}
-        className='rounded-full border border-gray-500 object-cover w-16 h-16 md:w-24 md:h-24 xl:w-26 xl:h-26 2xl:w-32 2xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='rounded-full border border-gray-500 object-cover w-16 h-16 md:w-24 md:h-24 xl:w-26 xl:h-26 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
-      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 easy-in-out group-hover:bg-white w-16 h-16 md:w-24 md:h-24 xl:w-26 xl:h-26 2xl:w-32 2xl:h-32 rounded-full z-0' >
+      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 easy-in-out group-hover:bg-white w-16 h-16 md:w-24 md:h-24 xl:w-26 xl:h-26 rounded-full z-0' >
         <div className='flex items-center justify-center h-full' >
           <p className='text-3xl font-bold text-black opacity-100' >
             {skill.progress}%
